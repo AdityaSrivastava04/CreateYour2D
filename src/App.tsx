b9c2613f07d323fe359c97883cc3583b1a88b9c0
Aditya Srivastava <adityasrvastava2004@gmail.com>
2025-12-02 21:12:45 +0530

    Setting up Vite TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
